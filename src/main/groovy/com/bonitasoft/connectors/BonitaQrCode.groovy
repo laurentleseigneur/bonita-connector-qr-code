ba133e520116cd594d85665492937876418cbc91
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2020-09-28 13:03:50 +0200

    add support for code 128 barcode type (#9)
    * add support for code 128 barcode type
    * add connector parameters

--- src/main/groovy/com/bonitasoft/connectors/BonitaQrCode.groovy
+++ src/main/groovy/com/bonitasoft/connectors/BonitaQrCode.groovy
@@ -1,5 +1,6 @@
 package com.bonitasoft.connectors
 
+import com.google.zxing.BarcodeFormat
 import org.bonitasoft.engine.bpm.document.DocumentValue
 import org.bonitasoft.engine.connector.AbstractConnector;
 import org.bonitasoft.engine.connector.ConnectorException;
@@ -9,13 +10,17 @@ import groovy.util.logging.Slf4j
 
 @Slf4j
 class BonitaQrCode extends AbstractConnector {
-    
+
+    def static final CODE_TYPE_INPUT = "codeType"
+
     def static final CONTENT_INPUT = "content"
     def static final FILENAME_INPUT = "filename"
     def static final SIZE_INPUT = "size"
+    def static final WIDTH_INPUT = "width"
+    def static final HEIGHT_INPUT = "height"
 
     def static final OUTPUT_DOCUMENT_VALUE = "outputDocumentValue"
-    
+
     /**
      * Perform validation on the inputs defined on the connector definition (src/main/resources/bonita-qr-code.def)
      * You should:
@@ -24,11 +29,23 @@ class BonitaQrCode extends AbstractConnector {
      */
     @Override
     void validateInputParameters() throws ConnectorValidationException {
+        checkMandatoryStringInput(CODE_TYPE_INPUT)
         checkMandatoryStringInput(CONTENT_INPUT)
         checkMandatoryStringInput(FILENAME_INPUT)
-        checkMandatoryIntegerInput(SIZE_INPUT)
+        def codeType = getInputParameter(CODE_TYPE_INPUT)
+        switch (codeType) {
+            case BarcodeFormat.QR_CODE.name():
+                checkMandatoryIntegerInput(SIZE_INPUT)
+                break
+            case BarcodeFormat.CODE_128.name():
+                checkMandatoryIntegerInput(WIDTH_INPUT)
+                checkMandatoryIntegerInput(HEIGHT_INPUT)
+                break
+            default:
+                throw new ConnectorValidationException(this, "Parameter $CODE_TYPE_INPUT accepts only '${BarcodeFormat.QR_CODE}' or '${BarcodeFormat.CODE_128}' value.")
+        }
     }
-    
+
     def checkMandatoryStringInput(inputName) throws ConnectorValidationException {
         def value = getInputParameter(inputName)
         if (value in String) {
@@ -58,31 +75,63 @@ class BonitaQrCode extends AbstractConnector {
      */
     @Override
     void executeBusinessLogic() throws ConnectorException {
-        def content = getInputParameter(CONTENT_INPUT)
+        def generatedCode
+        def codeType = getInputParameter(CODE_TYPE_INPUT)
         def filename = getInputParameter(FILENAME_INPUT)
+
+        switch (codeType) {
+            case BarcodeFormat.QR_CODE.name():
+                generatedCode = generateQrCode(filename)
+                break
+            case BarcodeFormat.CODE_128.name():
+                generatedCode = generateCode128(filename)
+                break
+        }
+
+        log.info("generate $codeType: $filename result based on ${generatedCode.getAbsolutePath()}")
+        DocumentValue documentValue = new DocumentValue(generatedCode.bytes, "image/png", filename)
+
+        setOutputParameter(OUTPUT_DOCUMENT_VALUE, documentValue)
+    }
+
+    File generateQrCode(def filename) {
+        def content = getInputParameter(CONTENT_INPUT)
+
         def size = getInputParameter(SIZE_INPUT)
         log.info "$CONTENT_INPUT : $content"
         log.info "$SIZE_INPUT : $size"
         log.info "$FILENAME_INPUT : $filename"
 
-        QrCodeGenerator generator=new QrCodeGenerator()
-        def qrCode = generator.generateQRCodeImage(content, size)
-
-        log.info("generate file $filename result based on ${qrCode.getAbsolutePath()}")
-        DocumentValue documentValue= new DocumentValue(qrCode.bytes,"image/png",filename)
-
-        setOutputParameter(OUTPUT_DOCUMENT_VALUE, documentValue)
+        QrCodeGenerator generator = new QrCodeGenerator()
+        File generatedCode = generator.generateQRCodeImage(content, size)
+        generatedCode
     }
-    
+
+    File generateCode128(def filename) {
+        def content = getInputParameter(CONTENT_INPUT)
+
+        def width = getInputParameter(WIDTH_INPUT)
+        def height = getInputParameter(HEIGHT_INPUT)
+
+        log.info "$CONTENT_INPUT : $content"
+        log.info "$WIDTH_INPUT : $width"
+        log.info "$HEIGHT_INPUT : $height"
+        log.info "$FILENAME_INPUT : $filename"
+
+        Code128Generator generator = new Code128Generator()
+        File generatedCode = generator.generateCode128Image(content, width, height)
+        generatedCode
+    }
+
     /**
      * [Optional] Open a connection to remote server
      */
     @Override
-    void connect() throws ConnectorException{}
+    void connect() throws ConnectorException {}
 
     /**
      * [Optional] Close connection to remote server
      */
     @Override
-    void disconnect() throws ConnectorException{}
+    void disconnect() throws ConnectorException {}
 }
